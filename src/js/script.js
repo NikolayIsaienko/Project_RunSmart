999515d25b1cf518545535673ab75e17436495c9
Nikolay Isaienko <nikolay.isaienko@gmail.com>
2020-10-18 17:16:41 +0300

    some editing

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
-        speed: 1000,        
+        speed: 1000,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
+        responsive: [{
             breakpoint: 992,
             settings: {
                 dots: true,
                 arrows: false
             }
-            }
-        ]
-      });
-  });
+        }]
+    });
+});
