406a8998ed3b963de86ceeee55fed2febf22d336
Nikolay Isaienko <nikolay.isaienko@gmail.com>
2020-11-08 18:29:27 +0200

    created modal windows and css rules for it

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
         }]
     });
 
+    // Catalog
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
             .addClass('catalog__tab_active')
@@ -37,4 +38,52 @@ $(document).ready(function () {
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    // Validation
+    function validationForm(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 5
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please specify your name",
+                    minlength: jQuery.validator.format("At least {0} characters required!")
+                },
+                phone: "Enter your phone number",
+                email: {
+                    required: "We need your email address to contact you",
+                    email: "Your email address must be in the format of name@domain.com"
+                }
+            }
+        });
+    };
+    validationForm('#consultation-form');
+    validationForm('#consultation form');
+    validationForm('#order form');
+
+    $('input[name=phone]').mask("+38 (999) 999-99-99");
 });
